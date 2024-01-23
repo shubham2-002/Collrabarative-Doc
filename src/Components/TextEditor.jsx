ac442599ef8cc4ec22523d8fe46459a4e91e48ae
Shubham Umb <shubham24.8@yahoo.com>
2024-01-23 19:09:11 +0530

    done realtime editing

--- src/Components/TextEditor.jsx
+++ src/Components/TextEditor.jsx
@@ -4,30 +4,40 @@ import "../App.css";
 
 const TextEditor = ({ socketRef, roomid }) => {
   const wrappeRef = useRef();
-  const [quill, SetQuill] = useState();
+  const quillRef= useRef(null)
 
   useEffect(() => {
+    let q;
     async function init() {
       const editor = document.createElement("div");
       wrappeRef.current.append(editor);
-      const q = new Quill(editor, { theme: "snow" });
-      SetQuill(q);
+     const q = new Quill(editor, { theme: "snow" });
+      quillRef.current=q;
+      console.log(quillRef.current)
 
       const handler = (delta, oldDelta, source) => {
         if (source !== "user") return;
         socketRef.current.emit("send-changes", { delta, roomid });
       };
-      q.on("text-change", handler)
-      
+      q.on("text-change", handler);
 
       return () => {
         wrappeRef.innerHTML = "";
-        q.off('text-change',handler)
+        q.off("text-change", handler);
       };
     }
 
     init();
-  }, [socketRef, roomid]);
+  }, []);
+
+  useEffect(() => {
+    if (socketRef.current) {
+      socketRef.current.on("receive-changes", ({ delta }) => {
+        console.log("recied change",delta)
+        quillRef.current.updateContents(delta);
+      });
+    }
+  }, [socketRef.current]);
   return <div id="container" ref={wrappeRef}></div>;
 };
 
